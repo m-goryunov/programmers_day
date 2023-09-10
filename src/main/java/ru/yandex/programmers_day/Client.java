c6c8312ea5843f965b75aba7b0820fe218fe9365
m.goryunov <goryunov93@yandex.ru>
2023-09-10 20:32:44 +0300

    feat: 2nd task

--- src/main/java/ru/yandex/programmers_day/Client.java
+++ src/main/java/ru/yandex/programmers_day/Client.java
@@ -1,11 +1,18 @@
 package ru.yandex.programmers_day;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 public class Client {
 
@@ -15,6 +22,65 @@ public class Client {
 
     public Client(String url) {
         this.url = url;
+    }
+
+    public void sendDecodedString() throws IOException, InterruptedException {
+
+        String json = "{\"decoded\": \"" + getAndDecodeString() + "\"}";
+
+        HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(json);
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(body)
+                .uri(URI.create("http://ya.praktikum.fvds.ru:8080/dev-day/task/2"))
+                .header("AUTH_TOKEN", "e4dfc14a-9afb-4867-94d2-29351cc15431")
+                .header("content-type", "application/json")
+                .build();
+
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        HttpResponse<String> response = client.send(request, handler);
+
+        System.out.println("Код ответа: " + response.statusCode());
+        System.out.println("Тело ответа: " + response.body());
+    }
+
+    private String getAndDecodeString() throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://ya.praktikum.fvds.ru:8080/dev-day/task/2"))
+                .header("AUTH_TOKEN", "e4dfc14a-9afb-4867-94d2-29351cc15431")
+                .header("content-type", "application/json")
+                .build();
+
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        HttpResponse<String> response = client.send(request, handler);
+
+        Document doc = Jsoup.parse(response.body());
+        Element codeElement = doc.select("code#message").first();
+        String result = codeElement.text();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(result);
+        String encoded = rootNode.get("encoded").asText();
+        int offset = Integer.parseInt(rootNode.get("offset").asText());
+        return decode(encoded, offset);
+    }
+
+    private String decode(String message, int offset) {
+        offset = offset % 26;
+        final byte spaceCode = (byte) 32;
+        byte[] bytes = message.getBytes(StandardCharsets.UTF_8);
+        for (int i = 0; i < bytes.length; i++) {
+            if (bytes[i] == spaceCode) {
+                continue;
+            }
+            if (bytes[i] - (byte) offset < 65) {
+                bytes[i] = (byte) (91 - (offset - (bytes[i] - 65)));
+            } else {
+                bytes[i] = (byte) (bytes[i] - (byte) offset);
+            }
+        }
+        return new String(bytes, StandardCharsets.UTF_8);
     }
 
     public void register() throws IOException, InterruptedException {
@@ -61,15 +127,8 @@ public class Client {
 
         HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
         HttpResponse<String> response = client.send(request, handler);
-        
+
         System.out.println("Код ответа: " + response.statusCode());
         System.out.println("Тело ответа: " + response.body());
     }
 }
-
-
-
-
-
-
-
--- src/main/java/ru/yandex/programmers_day/ProgrammersDayApplication.java
+++ src/main/java/ru/yandex/programmers_day/ProgrammersDayApplication.java
@@ -1,13 +1,10 @@
 package ru.yandex.programmers_day;
 
-//@SpringBootApplication
-/*SpringApplication.run(ProgrammersDayApplication.class, args);*/
 public class ProgrammersDayApplication {
 
 	public static void main(String[] args) throws Exception {
 
 		Client client = new Client("http://ya.praktikum.fvds.ru:8080/dev-day");
-		client.register();
-
+		client.sendDecodedString();
 	}
 }
