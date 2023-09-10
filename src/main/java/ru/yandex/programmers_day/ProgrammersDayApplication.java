8eb43db63ef844ae3230110d0492aeddfe3f83a4
m.goryunov <goryunov93@yandex.ru>
2023-09-10 11:53:12 +0300

    initial commit

---
+++ src/main/java/ru/yandex/programmers_day/ProgrammersDayApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.programmers_day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammersDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammersDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yandex/programmers_day/ProgrammersDayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.programmers_day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammersDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
